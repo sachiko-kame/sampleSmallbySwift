f95516a631a9ace2900a7bf004a79043fee25af3
sachiko-kame <taiyousannsann2525sann@yahoo.co.jp>
2018-08-14 14:02:59 +0900

    3.String→Int

--- smallSample/ViewController.swift
+++ smallSample/ViewController.swift
@@ -12,6 +12,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let numText:String = "100"
+        print(Int(numText)!)
     }
 
     override func didReceiveMemoryWarning() {
