d809992191e3aecccc4e0da6eec34fbd9d560daa
sachiko-kame <taiyousannsann2525sann@yahoo.co.jp>
2018-08-14 12:58:15 +0900

    clean

--- smallSample/ViewController.swift
+++ smallSample/ViewController.swift
@@ -9,32 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-    let animals:Array<aString> = [.dog, .cat, .turtle]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        for one in animals{
-            switch one {
-            case .dog:
-                print("犬か")
-            case .cat:
-                print("猫か")
-            default:
-                print("亀かもしれない")
-            }
-        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-
-}
-
-enum aString:String{
-    case dog = "犬"
-    case cat = "猫"
-    case turtle = "かめ"
 }
 
