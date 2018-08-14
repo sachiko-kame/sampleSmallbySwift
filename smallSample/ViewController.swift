04e243dce42a622bc3e81cc05aa7e8e427bb8243
sachiko-kame <taiyousannsann2525sann@yahoo.co.jp>
2018-08-14 15:18:28 +0900

    clean

--- smallSample/ViewController.swift
+++ smallSample/ViewController.swift
@@ -10,36 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let child = Child()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        child.bar()
-        child.foo()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-
 }
-
-protocol MyInterface {
-    func bar()
-    func foo()
-}
-
-class Child: MyInterface {
-    func bar() {
-        print("バー")
-    }
-    
-    func foo() {
-        print("ふー")
-    }
-}
-
-
-
 
