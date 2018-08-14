447f654a75ec6678a9efdc1d71b20ce1077f22c2
sachiko-kame <taiyousannsann2525sann@yahoo.co.jp>
2018-08-14 13:39:50 +0900

    2,Extension

--- smallSample/ViewController.swift
+++ smallSample/ViewController.swift
@@ -10,12 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let sampleText:String = "もしもし"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        sampleText.text()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
 }
-
+extension String{
+    func text(){
+        print("自分のテキストは\(self)")
+    }
+}
